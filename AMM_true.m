d4d2bf4fe8b0dac32bcf001b25784bbbd4f299dd
Chuhan Yang <cy1004@nyu.edu>
2022-06-20 16:18:20 +0400

    add function of optimal block AMM

---
+++ AMM_true.m
@@ -0,0 +1,19 @@
+function [Tr,m_list] = AMM_true(A,B,m,index_p)
+% function AMM (Approximate Matrix Multiplication) uses sampling algorithm.
+%  Inputs:
+%   m: number of sampling/outer product (usually m << order of T)
+
+k = length(index_p);
+Tr = zeros(size(A,1),size(B,2));
+% Calculate probability p
+p = zeros(k,1);
+for j = 1:1:k
+    p(j) = norm(A(:,index_p{j})*B(index_p{j},:));
+end
+p = p/sum(p);
+m_list = randsample(k,m,true,p);
+for j = 1:1:m
+    t = m_list(j);
+    Tr = Tr + A(:,t)*B(t,:)/p(t);
+end
+Tr = Tr/m;
