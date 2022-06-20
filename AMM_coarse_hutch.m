e2a7dc658d2af312db78240d4513b448402ddf7b
Chuhan Yang <cy1004@nyu.edu>
2022-06-20 16:17:48 +0400

    simplify the code

--- AMM_coarse_hutch.m
+++ AMM_coarse_hutch.m
@@ -8,17 +8,14 @@ function [Tr,m_list] = AMM_coarse_hutch(A,B,m,c,index_p)
 %   c: hyperparameter from hucthplusplus/simple_hutchinson
 %   index_p: cell arrays of index partition, eg. {[1,3,5],2,[2]}
 
-R_tran = A;
-R = B;
 k = length(index_p);
 Tr = zeros(size(A,1),size(B,2));
 % Calculate probability p
 p = zeros(k,1);
 W = cell(1,k); % create a cell array to store Submatrix group Product
 for j = 1:1:k
-    W{j} = R_tran(:,index_p{j})*R(index_p{j},:);
-    p(j) = simple_hutchinson(transpose(W{j})*W{j},c);
-    p(j) = sqrt(p(j));
+    W{j} = A(:,index_p{j})*B(index_p{j},:);
+    p(j) = sqrt(simple_hutchinson(transpose(W{j})*W{j},c));
 end
 p = p/sum(p);
 m_list = randsample(k,m,true,p);
