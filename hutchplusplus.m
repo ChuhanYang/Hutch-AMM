fa63cc2557bc918fb9f232d2fb18523757a90149
ChuhanYang <49384362+ChuhanYang@users.noreply.github.com>
2023-05-02 10:30:12 +0400

    Delete hutchplusplus.m
    Delete unneeded code.

--- hutchplusplus.m
+++
@@ -1,9 +0,0 @@
-% code by RaphaelArkadyMeyerNYU from https://github.com/RaphaelArkadyMeyerNYU/HutchPlusPlus
-
-function T = hutchplusplus(A,m)
-    S = 2*randi(2,size(A,1),m/3);
-    G = 2*randi(2,size(A,1),m/3);
-    [Q,~]=qr(A*S,0);
-    G = G - Q*(Q'*G);
-    T = trace(Q'*A*Q)+1/size(G,2)*trace(G'*A*G);
-end
